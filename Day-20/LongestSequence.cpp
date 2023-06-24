dc24cb94dbd84ddc8ef4e1ed94b970d7daa0ee2a
Yogesh Nanavare <94669580+yogesh123gn@users.noreply.github.com>
2023-06-24 23:17:39 +0530

    Add files via upload

---
+++ Day-20/LongestSequence.cpp
@@ -0,0 +1,33 @@
+/* Problem : Longest Common sequence
+
+Time Complexity : O()
+Space Complexity : O(N)
+
+*/
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+    set < int > hashSet;
+  for (int num: nums) {
+    hashSet.insert(num);
+  }
+
+  int longestStreak = 0;
+
+  for (int num: nums) {
+    if (!hashSet.count(num - 1)) {
+      int currentNum = num;
+      int currentStreak = 1;
+
+      while (hashSet.count(currentNum + 1)) {
+        currentNum += 1;
+        currentStreak += 1;
+      }
+
+      longestStreak = max(longestStreak, currentStreak);
+    }
+    }
+    return longestStreak;
+    }
+};
