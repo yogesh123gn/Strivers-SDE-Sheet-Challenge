0a2ce77d3abc4727704d8858c42096db14b12715
Yogesh Nanavare <94669580+yogesh123gn@users.noreply.github.com>
2023-07-02 19:26:07 +0530

    Add files via upload

---
+++ Day-27/DetectCycleinLinkedList.cpp
@@ -0,0 +1,28 @@
+/* Problem : Detect Cycle In the LinkedList
+Time Complexity : O(N)
+Space Complexity :O(1)
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == NULL) return false;
+        ListNode* fast = head;
+        ListNode* slow = head;
+        
+    while(fast->next != NULL && fast->next->next != NULL) {
+        fast = fast->next->next;
+        slow = slow->next;
+        if(fast == slow) return true;
+    }
+    return false;
+    }
+};
